2398da22b2ecbb592d47462fa75e8883cafda2dc
Andrew Scott <sixstorm@gmail.com>
2024-12-05 11:29:27 -0600

    Day 4 - COMPLETE

--- Day4/4.py
+++ Day4/4.py
@@ -5,8 +5,6 @@
 import time
 import re
 
-# 980 = Too low
-
 # Part 1
 # How many times does XMAS appear with the rules above?
 
@@ -15,17 +13,13 @@ with open("./4.txt", "r") as file:
 
 input = []
 totalCount = 0
+part2Count = 0
 
 for line in input_data:
     tempLine = [i for i in line if i != "\n"]
     input.append(tempLine)
 
 print(input)
-
-# x = 0
-# y = 2
-# print(f"{x=}, {y=}")
-# print(input[x][y])
 
 
 def check_grid2(line, input, x, y):
@@ -136,49 +130,70 @@ def check_grid2(line, input, x, y):
             print("Grid error")
 
 
-def check_ahead(line):
-    global totalCount
-    straight_occur = re.findall("(?=(XMAS))", line)
-    print(f"Found {len(straight_occur)} in {line}")
-    if straight_occur:
-        totalCount += 1
+def part2(input, x, y):
+    # Focused on the "A", look diagnally for M and S.
+    # X must be 1 down from top or 1 up from bottom
+    # Y must be 1 from the beginning or end of input
+    global part2Count
 
+    if (x >= 1) and (y >= 1):
+        print("Attemping to check A")
+        try:
+            if (input[x - 1][y - 1] == "M" and input[x - 1][y + 1] == "S") and (
+                input[x + 1][y - 1] == "M" and input[x + 1][y + 1] == "S"
+            ):
+                print(f"Found crossing at {x},{y}")
+                part2Count += 1
+        except IndexError:
+            print("Grid error")
 
-def check_backwards(line):
-    global totalCount
-    backwards = re.findall("(?=(SAMX))", line)
-    print(f"Found {len(backwards)} backwards in {line}")
-    if backwards:
-        totalCount += 1
+        try:
+
+            if (input[x - 1][y - 1] == "S" and input[x - 1][y + 1] == "S") and (
+                input[x + 1][y - 1] == "M" and input[x + 1][y + 1] == "M"
+            ):
+                print(f"Found crossing at {x},{y}")
+                part2Count += 1
+        except IndexError:
+            print("Grid error")
+
+        try:
+            if (input[x - 1][y - 1] == "M" and input[x - 1][y + 1] == "M") and (
+                input[x + 1][y - 1] == "S" and input[x + 1][y + 1] == "S"
+            ):
+                print(f"Found crossing at {x},{y}")
+                part2Count += 1
+        except IndexError:
+            print("Grid error")
+
+        try:
+            if (input[x - 1][y - 1] == "S" and input[x - 1][y + 1] == "M") and (
+                input[x + 1][y - 1] == "S" and input[x + 1][y + 1] == "M"
+            ):
+                print(f"Found crossing at {x},{y}")
+                part2Count += 1
+        except IndexError:
+            print("Grid error")
 
 
 x = 0
 y = 0
-
-# Straight through and backwards
-# for line in input_data:
-# check_ahead(line)
-# check_backwards(line)
-# print(f"{totalCount=}")
 
 for line in input:
     print("")
     print(line)
 
     for single in line:
-        # print(single)
-        if single == "X":
+        #        if single == "X":
+        #            print("")
+        #            check_grid2(line, input, x, y)
+        if single == "A":
             print("")
-            # print(f"{x},{y} is X")
-            check_grid2(line, input, x, y)
+            print(f"A at {x},{y}")
+            part2(input, x, y)
         y += 1
     x += 1
     y = 0
 
 print(f"{totalCount=}")
-
-# for y in input[x]:
-#    print(f"{y=}")
-#    check_grid(input, x, y)
-#    time.sleep(2)
-#    x += 1
+print(f"{part2Count=}")
