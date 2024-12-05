723d5939658dea289170cc54b38031b736f34b23
Andrew Scott <sixstorm@gmail.com>
2024-12-05 16:14:20 -0600

    Day 5 - Part 2 Incomplete

---
+++ Day5/5.py
@@ -0,0 +1,118 @@
+# Day 5
+# Safety protocols must be in a specific order
+#
+# Page Ordering Rules
+# Example: 47|53
+# If update includes page 47 and 53, then 47 must be printed before 53
+#
+# Updates
+# Example: 75,47,61,53,29
+# Page 75 must be printed before 47 (75|45), 75 before 61 (75|61), etc
+#
+# Middle page
+# If order is correct, you must find the middle page and add to sum
+# 75,47,61,53,29 - 61 is the middle page
+#
+
+import re
+import time
+
+with open("./example.txt", "r") as file:
+    input_data = file.readlines()
+
+# Variables
+correctUpdates = []
+incorrectUpdates = []
+middleNumbers = []
+
+
+def part1():
+    totalSum = 0
+    # Separate Rules from Updates
+    pors = [i.rstrip() for i in input_data if "|" in i]
+    updates = [i.rstrip() for i in input_data if "," in i]
+
+    print(pors)
+
+    for update in updates:
+        correct = True
+        # Iterate through each number, checking if it exists in pors list
+        pages = update.rstrip().split(",")
+        print(pages)
+
+        pagesLength = len(pages)
+        print(f"Update has {pagesLength} pages")
+
+        # Start 1 ahead to make it work
+        index = 1
+        for page in pages:
+            for p in pages[index:]:
+                rule = f"{page}|{p}"
+                if rule in pors:
+                    print(f"{rule} was found in PORS")
+                else:
+                    correct = False
+                    print(f"{rule} was NOT found in PORS")
+
+            index += 1
+        if correct == True:
+            correctUpdates.append(update)
+            middleNumbers.append(pages[len(pages) // 2])
+            totalSum += int(pages[len(pages) // 2])
+
+    print(correctUpdates)
+
+    print("")
+    print("Middle Numbers:")
+    print(middleNumbers)
+    print(f"{totalSum=}")
+
+
+def part2():
+    # Separate Rules from Updates
+    pors = [i.rstrip() for i in input_data if "|" in i]
+    updates = [i.rstrip() for i in input_data if "," in i]
+
+    for update in updates:
+        correct = True
+        pages = update.rstrip().split(",")
+        index = 1
+        for page in pages:
+            for p in pages[index:]:
+                rule = f"{page}|{p}"
+                if rule not in pors:
+                    correct = False
+            index += 1
+        if correct == True:
+            continue
+        else:
+            print(f"Adding {update} to incorrectUpdates")
+            incorrectUpdates.append(pages)
+
+    # Sort and correct
+    for update in incorrectUpdates:
+        # Go through each number
+        correct = False
+        i1 = 0
+        i2 = 1
+
+        while not correct:
+            while i2 < len(update):
+                rule = f"{update[i1]}|{update[i2]}"
+                if rule in pors:
+                    print(f"{rule} has been found in PORS")
+                else:
+                    print(
+                        f"{rule} is not in PORS; swapping {update[i2]} with {update[i1]}"
+                    )
+                    update[i1], update[i2] = update[i2], update[i1]
+                    print(update)
+                    time.sleep(2)
+                    break
+                i1 += 1
+                i2 += 1
+            index += 1
+
+
+# part1()
+part2()
