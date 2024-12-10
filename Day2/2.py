f3e01ac09f1c5b3059d1130342a725b6d6f66fca
Andrew Scott <sixstorm@gmail.com>
2024-12-10 08:58:00 -0600

    Day 2 COMPLETE

--- Day2/2.py
+++ Day2/2.py
@@ -8,15 +8,13 @@
 # 1. Levels are either all increasing or all decreasing
 # 2. Any two adjacent levels differ by at least one and at most three
 
+import time
 
-example = [
-    [7, 6, 4, 2, 1],
-    [1, 2, 7, 8, 9],
-    [9, 7, 6, 2, 1],
-    [1, 3, 2, 4, 5],
-    [8, 6, 4, 4, 1],
-    [1, 3, 6, 7, 9],
-]
+with open("./example.txt", "r") as file:
+    example = file.readlines()
+
+with open("./2.txt", "r") as file:
+    pInput = file.readlines()
 
 safeReports = []
 unsafeReports = []
@@ -46,77 +44,126 @@ def check_2(inputList):
 
     return result
 
-
-# Read in text file with input
-with open("example.txt", "r") as file:
-    input_data = file.readlines()
-
-# Read in 1 line at a time
-for line in input_data:
-    # Convert string into list of ints
-    line = list(line.rstrip().split(" "))
-    line = [int(i) for i in line]
-    print(line)
-
-    check1 = check_1(line)
-    print(f"Check1: {check1}")
-    check2 = check_2(line)
-    print(f"Check2: {check2}")
-
-    if check1 and check2:
-        safeReports.append(line)
-    else:
-        unsafeReports.append(line)
-
-print("")
-print(f"Safe Reports: {len(safeReports)}")
-print(safeReports)
-print(f"Unsafe Reports: {len(unsafeReports)}")
-print(unsafeReports)
-
-
-# Part 2
-# If one level can be removed, does it make it safe?
-print("")
-print("Part 2")
-print("")
-
-for line in unsafeReports:
-    print(f"Line: {line}")
-
-    # Loop through each possibility
-    x = 0
-    for level in line:
-        print("")
-        print(f"X: {x}")
-        # Create a tempLine where index x is removed
-        # tempLine = [i for i in line if i != line[x]]
-        tempLine = line
-        tempLine.remove(level)
-        print(f"Removing {level}")
-        print(f"TempLine: {tempLine}")
+def part1(input):
+    for line in input:
+        inputList = [int(x) for x in line.rstrip().split(" ")]
 
         # Check 1
-        check1 = check_1(tempLine)
-        print(f"Check 1 Results: {check1}")
+        check1 = check_1(inputList)
+        print(check1)
 
         # Check 2
-        check2 = check_2(tempLine)
-        print(f"Check 2 Results: {check2}")
+        check2 = check_2(inputList)
+        print(check2)
 
+        # Final Check
         if check1 and check2:
-            unsafeReports = [l for l in unsafeReports if l != line]
-            print("Unsafe")
-            print(unsafeReports)
             safeReports.append(line)
-            print("Safe")
-            print(safeReports)
-            print("Breaking on 2")
-            break
-        x += 1
+        else:
+            unsafeReports.append(line)
 
-print("")
-print(f"Safe Reports: {len(safeReports)}")
-print(safeReports)
-print(f"Unsafe Reports: {len(unsafeReports)}")
-print(unsafeReports)
+    print(len(safeReports))
+
+def part2(unsafeReports):
+    print("Part 2")
+    print(f"{unsafeReports}")
+    for line in unsafeReports:
+        inputList = [int(x) for x in line.rstrip().split(" ")]
+
+        for i, value in enumerate(inputList):
+            print(f"{i}-{value}")
+            tempList = inputList[:i]+inputList[i+1:]
+            print(f"{tempList=}")
+
+            # Check 1
+            check1 = check_1(tempList)
+            print(f"{check1=}")
+
+            # Check 2
+            check2 = check_2(tempList)
+            print(f"{check2=}")
+
+            if check1 and check2:
+                safeReports.append(line)
+                break
+
+
+                time.sleep(1)
+    print(f"Safe Reports: {len(safeReports)}")
+    print(f"{safeReports}")
+
+
+
+
+part1(pInput)
+part2(unsafeReports)
+
+# # Read in 1 line at a time
+# for line in input_data:
+#     # Convert string into list of ints
+#     line = list(line.rstrip().split(" "))
+#     line = [int(i) for i in line]
+#     print(line)
+
+#     check1 = check_1(line)
+#     print(f"Check1: {check1}")
+#     check2 = check_2(line)
+#     print(f"Check2: {check2}")
+
+#     if check1 and check2:
+#         safeReports.append(line)
+#     else:
+#         unsafeReports.append(line)
+
+# print("")
+# print(f"Safe Reports: {len(safeReports)}")
+# print(safeReports)
+# print(f"Unsafe Reports: {len(unsafeReports)}")
+# print(unsafeReports)
+
+
+# # Part 2
+# # If one level can be removed, does it make it safe?
+# print("")
+# print("Part 2")
+# print("")
+
+# for line in unsafeReports:
+#     print(f"Line: {line}")
+
+#     # Loop through each possibility
+#     x = 0
+#     for level in line:
+#         print("")
+#         print(f"X: {x}")
+#         # Create a tempLine where index x is removed
+#         # tempLine = [i for i in line if i != line[x]]
+#         tempLine = line
+#         tempLine.remove(level)
+#         print(f"Removing {level}")
+#         print(f"TempLine: {tempLine}")
+
+#         # Check 1
+#         check1 = check_1(tempLine)
+#         print(f"Check 1 Results: {check1}")
+
+#         # Check 2
+#         check2 = check_2(tempLine)
+#         print(f"Check 2 Results: {check2}")
+
+#         if check1 and check2:
+#             unsafeReports = [l for l in unsafeReports if l != line]
+#             print("Unsafe")
+#             print(unsafeReports)
+#             safeReports.append(line)
+#             print("Safe")
+#             print(safeReports)
+#             print("Breaking on 2")
+#             break
+#         x += 1
+
+# print("")
+# print(f"Safe Reports: {len(safeReports)}")
+# print(safeReports)
+# print(f"Unsafe Reports: {len(unsafeReports)}")
+# print(unsafeReports)
